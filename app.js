3f4496a54f295b4c0e42a72956f2a5fcb48f3771
Umang277 <71004884+Umang277@users.noreply.github.com>
2021-06-24 01:50:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var button= document.querySelector('.submit');
 
 button.addEventListener('click', function(name){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=04ecd9792714ad19052ab4abbe8a9b30')
-.then(response => response.json())
-.then(data => {
-  var tempValue = data['main']['temp'];
-  var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
+.then((apidata) => { return apidata.json()})//convert apidata(in json format) to js object so that we can work using js 
+.then((actualdata) => {//actual data is in js object 
+  var tempValue = actualdata['main']['temp'];
+  var nameValue = actualdata['name'];
+  var descValue = actualdata['weather'][0]['description'];
 
   main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
+  desc.innerHTML = "Desc - "+descValue;//display weather description
+  temp.innerHTML = "Temp - "+tempValue;//display temperature
   input.value ="";
 
 })
 
-.catch(err => alert("Wrong city name!"));
+.catch((err) => alert("wrong city name"));
 })
